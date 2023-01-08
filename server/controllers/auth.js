eb30e69d158e37d3bc7a5ada232f728bea08a616
ROBIN RILLON <rjrillon97@gmail.com>
2023-01-07 19:56:06 -0800

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import User from "../moodels/User.js"
+
+// REGISTER USER
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation 
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000)
+        });
+        const savedUser = await newUser.save();
+        // if register func DOESN'T error out, send the user back a status of 201 (something is created)
+        // and create a json version of the saved user so that the front end can receive the response. 
+        res.status(201).json(savedUser);
+    } catch (err) {
+        // 500 (something went wrong)
+        res.status(500).json({ error: err.message })
+    }
+};
+
+// LOGGING IN (very basic auth)
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({msg: "User does not exist. "});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. "});
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user});
+    } catch (err) {
+        res.status(500).json({error: err.message });
+    }
+}

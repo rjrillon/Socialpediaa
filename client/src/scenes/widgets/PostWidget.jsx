d8c2333aae4e1393e0cb64d4dbe004020deac631
ROBIN RILLON <rjrillon97@gmail.com>
2023-01-13 16:54:54 -0800

    Friend component for Friends List + Posts done

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,26 @@
+import { 
+    ChatBubbleOutlineOutlined, 
+    FavoriteBorderOutlines, 
+    FavoriteOutlined, 
+    ShareOutlined,
+} from "@mui/icons-material";
+import { 
+    Box,
+    Divider,
+    IconButton,
+    Typography,
+    useTheme,
+} from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend"
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
+
+const PostWidget = ({
+    
+})
+
+
+export default PostWidget;

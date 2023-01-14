40aa54d2ff1a8c98a6e0ee6e89d61f62bd6186eb
ROBIN RILLON <rjrillon97@gmail.com>
2023-01-13 17:36:11 -0800

    created and added advert widget to home

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdverWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
@@ -31,7 +32,8 @@ const HomePage = () => {
                     <PostsWidget userId={_id} />
                 </Box>
                 {isNonMobileScreens && <Box flexBasis="26%"></Box>}
-
+                <AdvertWidget/>
+                <Box m="2rem 0"/>
             </Box>
         </Box>
     );
